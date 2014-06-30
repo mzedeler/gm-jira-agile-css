b5e0d351dc8baf32397d2aa47bab291aa99370ec
Michael Zedeler <michael@zedeler.dk>
2014-06-30 22:06:15 +0200

    Added css.

--- gm-jira-agile-css.user.js
+++ gm-jira-agile-css.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Jira Agile issue print style
 // @namespace   http://github.com/mzedeler/gm-jira-agile-css
-// @version     0.2
+// @version     0.3
 // @description Add css styles to make printing issues from Jira
 // @match       https://notalib.atlassian.net/secure/RapidBoard.jspa?*view=planning*
 // @copyright   2014, Michael Zedeler <michael@zedeler.dk>
